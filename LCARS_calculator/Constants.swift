232a15d62b3ffffe32622af109c57422c4162552
AndrewLewis83 <andrewlewisdev@gmail.com>
2020-12-16 12:36:02 -0700

    Created extra storyboard for iPad, to be fleshed out in the future. For the iPhone, removed all imagesfrom number/function buttons and replaced with cornerRadius and background colors. Revised spacing. Added backspace button.

--- LCARS_calculator/Constants.swift
+++ LCARS_calculator/Constants.swift
@@ -15,7 +15,11 @@ let warpColorOne = UIColor(red: 248.0/255.0, green: 208.0/255.0, blue: 116.0/255
 let textColorOne = UIColor(red: 81.0/255.0, green: 157.0/255.0, blue: 202.0/255.0, alpha: 1.0)
 let textColorTwo = UIColor(red: 165.0/255.0, green: 165.0/255.0, blue: 206.0/255.0, alpha: 1.0)
 
-//let borderColor = UIColor(red: 249.0/255.0, green: 216.0/255.0, blue: 118.0/255.0, alpha: 1.0)
+let buttonColorOne = UIColor(red: 222.0/255.0, green: 189.0/255.0, blue: 99.0/255.0, alpha: 1.0)
+let buttonColorTwo = UIColor(red: 198.0/255.0, green: 181.0/255.0, blue: 239.0/255.0, alpha: 1.0)
+let buttonColorThree = UIColor(red: 255.0/255.0, green: 173.0/255.0, blue: 33.0/255.0, alpha: 1.0)
+let buttonColorFour = UIColor(red: 147.0/255.0, green: 111.0/255.0, blue: 138.0/255.0, alpha: 1.0)
+
 @available(iOS 13.0, *)
 let borderColor = CGColor(srgbRed: 255.0/255.0, green: 173.0/255.0, blue: 33.0/255.0, alpha: 1.0)
 
